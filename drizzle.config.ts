dc164bff0231be224846e549c6549e8de3b2c397
John Doe <117672208+3142akay@users.noreply.github.com>
2025-10-06 20:08:29 +0530

    02:database

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,11 @@
-import 'dotenv/config';
-import { defineConfig } from 'drizzle-kit';
+import "dotenv/config";
+import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  out: './drizzle',
-  schema: './src/db/schema.ts',
-  dialect: 'postgresql',
+  out: "./drizzle",
+  schema: "./src/db/schema.ts",
+  dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL || "postgresql://neondb_owner:npg_mH2hTACgE0pj@ep-lingering-cherry-adzgszv9-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+    url: process.env.DATABASE_URL!,
   },
 });
